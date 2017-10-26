ee97c8361d1574ec74d6d2690eaedf45abe77315
William Henney <will@henney.org>
2017-10-26 09:29:26 -0500

    Do multibinning at constant s/n for the MUSE weak ines

--- selected-multibin.sh
+++ selected-multibin.sh
@@ -1,4 +1,4 @@
 D=../multibin-maps
-for line in Ni_IV-5820 N_II-5680 N_II-5667 N_II-5942 O_II-4642 O_II-4676 O_I-7773; do
+for line in Ni_IV-5820 N_II-5680 N_II-5667 N_II-5942 O_II-4642 O_II-4676 O_I-7773 C_II-6462 C_II-6780; do
     python $D/multibin-map.py LineMaps/linesum-$line.fits
 done
